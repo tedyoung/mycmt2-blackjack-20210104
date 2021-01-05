86af8d1111488f56a83db9c610f85d26eda00cc1
Ted M. Young <tedyoung@gmail.com>
2021-01-05 15:36:56 -0800

    Lab 4a: write some tests for existing behavior with StubDeck.

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -8,13 +8,27 @@ class GameOutcomeTest {
 
   @Test
   public void playerBeatsDealerThenOutcomeIsPlayerWins() throws Exception {
-    Game game = new Game();
+    Deck stubDeck = new StubDeck(Rank.TEN, Rank.EIGHT, Rank.QUEEN, Rank.NINE);
+    Game game = new Game(stubDeck);
 
     game.initialDeal();
     game.playerStands();
     game.dealerTurn();
 
     assertThat(game.determineOutcome())
-        .isEqualTo("");
+        .isEqualTo("You beat the Dealer! \uD83D\uDCB5");
   }
+
+  @Test
+  public void playerGoesBustAndLoses() throws Exception {
+    Deck stubDeck = new StubDeck(Rank.TEN, Rank.EIGHT, Rank.QUEEN, Rank.SIX, Rank.THREE);
+    Game game = new Game(stubDeck);
+
+    game.initialDeal();
+    game.playerHits();
+
+    assertThat(game.determineOutcome())
+        .isEqualTo("You Busted, so you lose.  💸");
+  }
+
 }
